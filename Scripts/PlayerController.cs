ac121234bd366c722d300cf431be684c3e8c98a6
RichardNava <richard_nava@hotmail.com>
2021-10-06 10:00:20 +0200

    06/10/2021

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float runSpeed = 2;
-    public float jumpSpeed = 4;
+    // Atributos físicos para el movimiento (fuerza)
+    public float runSpeed = 2f;
+    public float jumpSpeed = 4f;
+    public float doubleJumpSpeed = 1.5f;
+
+
+    private bool canDoubleJump = false;
+   
     public Rigidbody2D rb2D;
 
     public SpriteRenderer spriteR;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D.GetComponent<Rigidbody2D>();
-
 
     }
 
@@ -25,11 +31,52 @@ public class PlayerController : MonoBehaviour
         {
             if (CheckGround.isGrounded)
             {
+                anim.SetBool("Jump", true);
                 rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+                canDoubleJump = true;
+            }
+            else
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    if (canDoubleJump)
+                    {
+                        anim.SetBool("DoubleJump", true);
+                        rb2D.velocity = new Vector2(rb2D.velocity.x, doubleJumpSpeed);
+                        canDoubleJump = false;
+                    }
+                }
             }
 
         }
+        if (CheckGround.isGrounded == false)
+        {
+            anim.SetBool("Jump", true);
+            anim.SetBool("Run", false);
+        }
+        if (CheckGround.isGrounded == true)
+        {
+            anim.SetBool("Jump", false);
+            anim.SetBool("DoubleJump", false);
+            anim.SetBool("Fall", false);
+
+        }
+        if (rb2D.velocity.y < 0)
+        {
+            anim.SetBool("Fall", true);
+
+            //if (Input.GetKey(""))
+            //{
+
+            //}
+        }
+        if (rb2D.velocity.y > 0 && !Input.GetKey("space"))
+        {
+            anim.SetBool("Fall", false);
+        }
     }
+    
+
 
     private void FixedUpdate()
     {
@@ -37,13 +84,19 @@ public class PlayerController : MonoBehaviour
         {
             rb2D.velocity = new Vector2(runSpeed,rb2D.velocity.y);
             spriteR.flipX = false;
-            Debug.Log("Sigo aqui");
+            anim.SetBool("Run", true);
 
         }
         else if (Input.GetKey("left") || Input.GetKey("a"))
         {
             rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
             spriteR.flipX = true;
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            anim.SetBool("Run", false);
         }
 
     }
